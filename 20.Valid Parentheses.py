2c4e5940e32d1bc8961b00ff3f80e26ab9a849c4
artemsprygin <artemsprygin@me.com>
2018-08-11 02:41:55 +0300

    add 20

---
+++ 20.Valid Parentheses.py
@@ -0,0 +1,14 @@
+class Solution:
+    # @return a boolean
+    def isValid(self, s):
+        stack, lookup = [], {"(": ")", "{": "}", "[": "]"}
+        for parenthese in s:
+            if parenthese in lookup:
+                stack.append(parenthese)
+            elif len(stack) == 0 or lookup[stack.pop()] != parenthese:
+                return False
+        return True
+
+if __name__ == "__main__":
+    print(Solution().isValid("()[]{}"))
+    print(Solution().isValid("()[{]}"))
